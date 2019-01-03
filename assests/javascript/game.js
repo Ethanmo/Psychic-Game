61f3a947753ff34ff6744f12d8c77e48192bffbd
Ethanmo <mythface00@gmail.com>
2019-01-03 14:50:19 -0800

    updated

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,43 +1,3 @@
-/* var game = {
-    wordList : [
-        "Bulbasaur",    
-        "Weedle",
-        "Geodude",
-        "Pikachu",
-        "Magikarp",
-        "Mewtwo"
-    ],
-
-    score : 0,
-
-    guess : function (answer) {
-        var result = "_"
-        for (var i = 1; i < answer.length; i++){
-            result = result + " _";
-        }
-        return result; 
-    },
-
-    wordPicker : function (){
-        return this.wordList[Math.floor(Math.random() * wordList.length)];
-    },
-
-    
-
-    compare : function (){
-        
-    }, 
-
-    setCharAt : function(string, index, chr){
-        if (index > (string.length - 1)){
-            return string;
-        }
-        return string.substring(0,index) + chr + string.substring(index + 1);
-    }   
-
-    
-} */
-
 // declare some variable before the game starts, some left empty for future use
 var wordList = [
     "Bulbasaur",    
@@ -53,58 +13,52 @@ var losses = 0;
 var answer = "";
 var current = "";
 var wrongGuesses = [];
-var guessesRemain = 10;
+var guessesRemain = 7;
 
 // pick a random word from wordlist
 function wordPicker(){
     return wordList[Math.floor(Math.random() * wordList.length)];
 }
 
-//non-prototype bulky version
-/* function setCharAt(string, i, char){
-    if (index > (string.length - 1)){
+// set the character at given index in a string to char
+function setCharAt(string, i, char){
+    if (i > (string.length - 1)){
         return string;
     }
-    return string.substring(0, i) + chr + string.substring(i + 1);
-} */
+    return string.substring(0, i) + char + string.substring(i + 1);
+} 
 
 // create prototype function for string to set char at certain index to given value
-String.prototype.setCharAt = function (i, char){
-    return this.substring(0, i) + chr + this.substring(i + 1);
-}
-
-//non-prototype bulky version
-/* function isLetter(char) {
-    return char.toLowerCase() != char.toUpperCase();
-} */
-
-//create prototype function for string to check if the given string is a letter
-String.prototype.isLetter = function (char){
-    return (char.toLowerCase() != char.toUpperCase() && char.length === 1);
-}
-
-//create prototype function for string to check if the string contains a certain character
-String.prototype.containsLetter = function(char){
+function containsLetter(word, char){
     var check = false;
-    for (var i = 0; i < this.length; i++){
-        if (this.charAt[i] === char){
+    for (var i = 0; i < word.length; i++){
+        if (word.charAt(i).toLowerCase() === char){
             check = true;
         }
     }
     return check;
 }
 
+//return true if the given char is a letter
+function isLetter(char){
+    if (char.toLowerCase() !== char.toUpperCase()){
+        if (char.length === 1){
+            return true;
+        }
+    }
+    return false;
+}
+
+
 //initiate the game by giving value to empty variables declared at the beginning
 function initiateGame(){
     answer = wordPicker(wordList);
-    current = function (){
-        var j = "_";
-        for (var i = 1; i < answer.length; i++){
-            j += " _";
-        }
-        return j;
+    console.log(answer);
+    for (var i = 0; i < answer.length; i++){
+        current += '_';
     }
     // set innerHTML to current.
+    document.getElementById('current').innerHTML = ' ' + current;
 }
 
 //compare the user input(guess) to each character in the answer string. 
@@ -112,7 +66,7 @@ function initiateGame(){
 //if wrong guess and the guess is not already in wrongGuesses, add to
 function update(guess){
     //if answer doesn't contain guess
-    if (!answer.containsLetter(guess)){
+    if (!(containsLetter(answer, guess))){
         //if wrongGuesses list doesn't contain guess, add it
         if (wrongGuesses.indexOf(guess) === -1){
             wrongGuesses.push(guess);
@@ -120,40 +74,61 @@ function update(guess){
         }
     } else {
         for (var i = 0; i < answer.length; i++){
-           if (answer.charAt[i] === guess){
-              current = current.setCharAt(i, guess);
+            if (answer.charAt(i).toLowerCase() === guess){
+                current = setCharAt(current, i, answer.charAt(i));
             } 
         }
     }
 }
 
-function gameOver(){
+//show the progress of the game to player
+function gameProgress(){
+    console.log("the answer is: " + answer);
+    console.log(current);
     if (answer === current) {
         //game over and win
         wins++;
         reset();
+        document.getElementById('current').innerHTML = "WIN";
     } else if(guessesRemain === 0) {
         //game over and loss
         losses++;
         reset();
+        document.getElementById('current').innerHTML = "LOSS";
+    } else {
+        document.getElementById('current').innerHTML = current;
+        document.getElementById('remain').innerHTML = guessesRemain;
+        document.getElementById('start').innerHTML = '';
     }
-    // display current, wrongGuesses,guessesRemain,wins,losses
+    
 }
 
+//when win or run out of guesses, reveal answer and reset the game
 function reset(){
-    var answer = "";
-    var current = "";
-    var wrongGuesses = [];
-    var guessesRemain = 10;
+    document.getElementById('remain').innerHTML = "The Pokemon was : " + answer;
+    answer = "";
+    current = "";
+    wrongGuesses = [];
+    guessesRemain = 10;
+    document.getElementById('start').innerHTML = 'Press any key to play again';
+    initiateGame();
 }
 
+//call the function to start the game
 initiateGame();
 
+//takes in the user input and update 
 document.onkeyup = function(event){
     var input = event.key.toLowerCase();
-    if (input.isLetter()){
+    console.log(input);
+    if (isLetter(input)){
         update(input);
-        gameOver();
+        gameProgress();
+        // display current, wrongGuesses,guessesRemain,wins,losses
+        document.getElementById('wrong').innerHTML = wrongGuesses;
+        document.getElementById('wins').innerHTML = "WINS : " + wins;
+        document.getElementById('losses').innerHTML ="LOSSES : " + losses;
     }
+
 
 }
